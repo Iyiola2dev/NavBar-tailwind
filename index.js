ff3cc04d8214a411bdc1a5302fe48773f5b929a3
Lexicon <iyiolad21@gmail.com>
2024-07-03 15:06:31 +0100

    slideer

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ function hideSidebar(){
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = 'none';
 }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const slides = document.querySelector(".slides");
+//   let counter = 0;
+
+//   setInterval(() => {
+//     counter++;
+//     slides.style.transform = `translateX(-${counter * 100 / 5}%)`; // Adjust for number of slides (5 in this case)
+    
+//     if (counter === 5) { // Adjust for number of slides (5 in this case)
+//       counter = 0;
+//       setTimeout(() => {
+//         slides.style.transition = "none";
+//         slides.style.transform = `translateX(0)`;
+//         setTimeout(() => {
+//           slides.style.transition = "transform 0.5s ease";
+//         });
+//       }, 500); // Adjust for transition duration
+//     }
+//   }, 3000); // Adjust for interval duration (3 seconds in this case)
+// });
